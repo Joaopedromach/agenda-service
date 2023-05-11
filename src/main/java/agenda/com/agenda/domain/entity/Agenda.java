516b3bccc91e04f92895469925a93c1bb0a14857
Joaopedromach <estranhonerd123@gmail.com>
2023-05-11 20:47:13 -0300

    Entity com Lombok

---
+++ src/main/java/agenda/com/agenda/domain/entity/Agenda.java
@@ -0,0 +1,27 @@
+package agenda.com.agenda.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@Entity
+@Table(name = "agenda")
+public class Agenda {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "descricao")
+    private String descricao;
+    @Column(name = "data_hora")
+    private LocalDateTime horario;
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+    //para registrar um por paciante o ManyToOne
+    @ManyToOne
+    private Paciente paciente;
+
+
+}
--- src/main/java/agenda/com/agenda/domain/entity/Paciente.java
+++ src/main/java/agenda/com/agenda/domain/entity/Paciente.java
@@ -1,7 +1,11 @@
 package agenda.com.agenda.domain.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
 @Table(name = "paciente")
 
@@ -13,44 +17,4 @@ public class Paciente {
     private String sobrenome;
     private String email;
     private String cpf;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getSobrenome() {
-        return sobrenome;
-    }
-
-    public void setSobrenome(String sobrenome) {
-        this.sobrenome = sobrenome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
 }
