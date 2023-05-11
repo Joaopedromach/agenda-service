f7e01d5b2f3602ff5d2071f22aef7f49878e9a8f
Joaopedromach <estranhonerd123@gmail.com>
2023-05-11 20:38:19 -0300

    Entity

---
+++ src/main/java/agenda/com/agenda/domain/entity/Paciente.java
@@ -0,0 +1,56 @@
+package agenda.com.agenda.domain.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "paciente")
+
+public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String sobrenome;
+    private String email;
+    private String cpf;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+}
