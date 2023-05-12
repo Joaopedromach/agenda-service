10f0365e41547a22ecc42fac3dec1e0bfb83c820
Joaopedromach <estranhonerd123@gmail.com>
2023-05-12 18:45:43 -0300

    Controller Listar

--- src/main/java/agenda/com/agenda/api/PacienteController.java
+++ src/main/java/agenda/com/agenda/api/PacienteController.java
@@ -8,6 +8,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/paciente")
@@ -18,5 +21,9 @@ public class PacienteController {
         Paciente pacienteSalvo = service.salvar(paciente);
         return ResponseEntity.status(HttpStatus.CREATED).body(pacienteSalvo);
 
+    }
+    public ResponseEntity <List<Paciente>> listarTodos(){
+        List<Paciente> pacientes = service.listarTodos();
+        return ResponseEntity.status(HttpStatus.OK).body(pacientes);
     }
 }
