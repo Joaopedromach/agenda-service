3ce4c8879df0fa624b18f3bbd7dc182c799c4214
Joaopedromach <estranhonerd123@gmail.com>
2023-05-12 18:31:21 -0300

    service

--- src/main/java/agenda/com/agenda/domain/repository/PacienteRepository.java
+++ src/main/java/agenda/com/agenda/domain/repository/PacienteRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PacienteRepository extends JpaRepository<Long, Paciente> {
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
 }
--- src/main/java/agenda/com/agenda/domain/service/PacienteService.java
+++ src/main/java/agenda/com/agenda/domain/service/PacienteService.java
@@ -1,3 +1,37 @@
 package agenda.com.agenda.domain.service;
+
+import agenda.com.agenda.domain.entity.Paciente;
+import agenda.com.agenda.domain.repository.PacienteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+//Para nao precisar criar o construtor eu uso o
+@RequiredArgsConstructor
 public class PacienteService {
+    //puxar o repository para passar o controle para o spring para nao passar o new eu dou o @Auto
+    //final depois que coloco ele nao pode receber outra por cima
+    private final PacienteRepository repository;
+    public Paciente salvar(Paciente paciente){
+        //TODO para validar se o cpf ja nao existe
+        repository.save(paciente);
+    }
+    public List<Paciente> listarTodos(){
+        return repository.findAll();
+    }
+    //para buscar por Id eu dou um optional e retorno a classe
+    public Optional<Paciente> buscarporId(Long id){
+        return repository.findById(id);
+
+    }
+
+    public void deletar(Long id){
+        repository.deleteById(id);
+    }
 }
