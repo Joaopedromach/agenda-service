b913339785026d20eadfbb2ba960dd2f26683c00
Joaopedromach <estranhonerd123@gmail.com>
2023-05-14 17:57:37 -0300

    Agenda Repository

---
+++ src/main/java/jpmac/com/agenda/domain/repository/AgendaRepository.java
@@ -0,0 +1,9 @@
+package jpmac.com.agenda.domain.repository;
+
+import jpmac.com.agenda.domain.entity.Agenda;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AgendaRepository extends JpaRepository<Agenda, Long> {
+}
